9641d0cb90fcb7c1ffaaf983a9bd5c83e3d51f24
Hugo Olivares <hdolivares97@gmail.com>
2024-04-12 11:57:34 -0600

    curso JAVA

--- src/com/aluracursos/radioalura/modelos/Cancion.java
+++ src/com/aluracursos/radioalura/modelos/Cancion.java
@@ -1,9 +1,18 @@
 package com.aluracursos.radioalura.modelos;
 
-public class Canciones extends Audio{
+public class Cancion extends Audio{
     private String album;
     private String cantante;
     private String genero;
+
+    @Override
+    public int getClasificacion() {
+        if(getTotalDeMeGusta() > 5000){
+            return 8;
+        } else {
+            return 4;
+        }
+    }
 
     public String getAlbum() {
         return album;
--- src/com/aluracursos/radioalura/principal/Principal.java
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -1,2 +1,40 @@
-package com.aluracursos.radioalura.principal;public class Principal {
+package com.aluracursos.radioalura.principal;
+
+import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.MisFavoritos;
+import com.aluracursos.radioalura.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("Kiss");
+
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Gabriela Aguiar");
+        miPodcast.setTitulo("Cafe.Tech");
+
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+        }
+
+        for (int i = 0; i < 2000; i++) {
+            miCancion.reproduce();
+        }
+
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
+        }
+
+        System.out.println("Total de reproducciones: " + miCancion.getTotalDeReproducciones());
+        System.out.println("Total de Me Gusta: " + miCancion.getTotalDeMeGusta());
+
+        MisFavoritos favoritos =  new MisFavoritos();
+        favoritos.adiciona(miPodcast);
+        favoritos.adiciona(miCancion);
+    }
 }
